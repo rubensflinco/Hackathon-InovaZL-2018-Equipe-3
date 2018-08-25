c5da58dc33d2ed5a4ac435cb06c541371e3e5d0e
JotinhaBR <planetacraftvip@hotmail.com.br>
2018-08-25 19:00:30 -0300

    mudando tudo

--- sistema/ANTIGO/sauderapida/routes/index.js
+++ sistema/ANTIGO/sauderapida/routes/index.js
@@ -1,11 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-connection.query("SELECT * FROM usuarios", function (err, result, fields) {
-  if (err) throw err;
-  console.log(result);
-});
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
